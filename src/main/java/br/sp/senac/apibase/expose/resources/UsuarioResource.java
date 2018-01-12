c311055d178e88744bbb4a4a937036bd37b63d55
Joaquim Neto <jsn.piata@gmail.com>
2018-01-12 10:35:12 -0200

    Configurado geração do swagger-ui

--- src/main/java/br/sp/senac/apibase/expose/resources/UsuarioResource.java
+++ src/main/java/br/sp/senac/apibase/expose/resources/UsuarioResource.java
@@ -2,15 +2,31 @@ package br.sp.senac.apibase.expose.resources;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
 
-import io.swagger.oas.annotations.Operation;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiResponse;
 
+@Api(value = "Usuario")
 @Path("/usuarios")
 public class UsuarioResource {
 
 	@GET
-	@Operation(summary = "Lista usuários", description = "lista todos os usuários cadastrados")
+	@ApiResponse(responseContainer = "list", code = 200, message = "")
 	public String teste() {
 		return "teste";
+	}
+
+	@GET
+	@Path("/{id}")
+	@ApiResponse(responseContainer = "list", code = 200, message = "")
+	public Object buscarPorId(@PathParam("id") Long id) {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://www.mocky.io/v2/5a4f9e482f00005b0e790c57");
+		
+		return target.request().get().readEntity(String.class);
 	}
 }
--- src/main/java/br/sp/senac/apibase/main/configuration/SwaggerConfig.java
+++ src/main/java/br/sp/senac/apibase/main/configuration/SwaggerConfig.java
@@ -1,11 +1,8 @@
 package br.sp.senac.apibase.main.configuration;
 
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import io.swagger.oas.integration.SwaggerConfiguration;
-import io.swagger.oas.models.OpenAPI;
-import io.swagger.oas.models.info.Info;
+import br.sp.senac.apibase.main.Ambiente;
+import br.sp.senac.apibase.main.SenacUriConventions;
+import io.swagger.jaxrs.config.BeanConfig;
 
 /**
  * @author Joaquim Neto
@@ -18,21 +15,12 @@ public class SwaggerConfig {
 	}
 
 	public void inicializar() {
-		OpenAPI openAPI = new OpenAPI();
-		Info info = new Info()
-				.title("Senac API - apibase")
-				.description("Projeto para para construção de APIs usando jax-rs");
-		
-		openAPI.info(info);
-		
-		SwaggerConfiguration config = new SwaggerConfiguration();
-		config.openAPI(openAPI).resourcePackages(Stream.of("br.sp.senac.apibase.expose.resources").collect(Collectors.toSet()));
-//		BeanConfig beanConfig = new BeanConfig();
-//		beanConfig.setTitle("Senac API - noticia-expose");
-//		beanConfig.setVersion("1.0.0");
-//		beanConfig.setHost(Ambiente.getPublicHost());
-//		beanConfig.setBasePath("/apibase" + SenacUriConventions.BASE_URL_API);
-//		beanConfig.setResourcePackage("br.sp.senac.apibase.expose.resources");
-//		beanConfig.setScan(true);
+		BeanConfig beanConfig = new BeanConfig();
+		beanConfig.setTitle("Senac API");
+		beanConfig.setVersion("1.0.0");
+		beanConfig.setHost(Ambiente.getPublicHost());
+		beanConfig.setBasePath("/apibase" + SenacUriConventions.BASE_URL_API);
+		beanConfig.setResourcePackage("br.sp.senac.apibase.expose.resources");
+		beanConfig.setScan(true);
 	}
 }
